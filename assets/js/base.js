3c5f0f4d160cb19249ffc88e3b69199da00a3e93
Troy Abrigo <83326212+Nipulse@users.noreply.github.com>
2021-12-09 11:49:40 +0800

    Into the web animate

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
   $(".entry-link").click(function(){
+      $('.entry-container div').fadeOut();
+      $('.home-text').fadeIn();
+      $('.entry-container').delay(700).css('background','url(https://media3.giphy.com/media/MaThe6p8WAKbf9NDDM/giphy.gif?cid=ecf05e47j9fbokhc2jpollyvs4gfcv5o2szymjq33w14lnwh&rid=giphy.gif&ct=g) no-repeat');
+      $('.entry-container').css('background-size','100vw 100vh');
       $('.navbar').slideDown(600);
   }); 
+
+  
   $("#backtotop").click(function(){
     $("html, body").animate({ scrollTop: 0 }, "slow");
     return false;
-}); 
+  }); 
 });
 
